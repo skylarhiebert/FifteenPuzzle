c8b000dd1f0d94f739f80faeaf32096c93e6c81a
Guest User <Guest@ecs325-17.labs.encs>
2012-02-03 12:26:07 -0800

    Initial Commit

---
+++ FifteenPuzzle/FifteenPuzzleAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  FifteenPuzzleAppDelegate.h
+//  FifteenPuzzle
+//
+//  Created by Guest User on 2/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FifteenPuzzleViewController;
+
+@interface FifteenPuzzleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) FifteenPuzzleViewController *viewController;
+
+@end
---
+++ FifteenPuzzle/FifteenPuzzleViewController.h
@@ -0,0 +1,13 @@
+//
+//  FifteenPuzzleViewController.h
+//  FifteenPuzzle
+//
+//  Created by Guest User on 2/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FifteenPuzzleViewController : UIViewController
+
+@end
